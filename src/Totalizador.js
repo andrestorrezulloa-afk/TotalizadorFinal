8083797b51c08f003218457bc8a5c4d7d55cba48
André Ermoth <andres.torrez.ulloa@gmail.com>
2026-09-08 23:35:28 -0400

    feat: Se agrego a los demas estados para el calculo de impuestos

--- src/Totalizador.js
+++ src/Totalizador.js
@@ -10,6 +10,10 @@ export function calcularPrecioNeto(cantidad, precio) {
 export function calcularImpuesto(precioNeto, estado) {
   const impuestosPorEstado = {
     "UT": 0.0665, 
+    "NV": 0.0800,
+    "TX": 0.0625, 
+    "AL": 0.0400, 
+    "CA": 0.0825  
   };
   if (!(estado in impuestosPorEstado)) {
     return "Estado inválido";
--- src/Totalizador.test.js
+++ src/Totalizador.test.js
@@ -1,5 +1,6 @@
 import { calcularPrecioNeto, calcularImpuesto} from "./Totalizador.js";
 
+//precio neto
 describe("Totalizador - Precio Neto", () => {
   it("deberia calcular el precio neto multiplicando la cantidad por el precio", () => {
     expect(calcularPrecioNeto(3, 20)).toEqual(60);
@@ -17,6 +18,14 @@ describe("Totalizador - Impuesto", () => {
   it("deberia calcular el monto del impuesto para el estado UT (6.65%)", () => {
     expect(calcularImpuesto(100, "UT")).toEqual(6.65);
   });
+  it("deberia calcular el monto del impuesto para el estado TX (6.25%)", () => {
+    expect(calcularImpuesto(100, "TX")).toEqual(6.25);
+  });
 
-  
+  it("deberia calcular el monto del impuesto para el estado CA (8.25%)", () => {
+    expect(calcularImpuesto(200, "CA")).toEqual(16.5);
+  });
+  it("debería retornar 'Estado inválido' si el código de estado no existe en la lista", () => {
+    expect(calcularImpuesto(100, "XX")).toEqual("Estado inválido");
+  });
 });
