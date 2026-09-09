12900fc70c7863ad5fd77e3aef280fa806313103
André Ermoth <andres.torrez.ulloa@gmail.com>
2026-09-08 23:22:24 -0400

    test:Se agrego 2 test mas para verificar las reglas de negocios al precio neto

--- src/Totalizador.test.js
+++ src/Totalizador.test.js
@@ -4,4 +4,10 @@ describe("Totalizador - Precio Neto", () => {
   it("deberia calcular el precio neto multiplicando la cantidad por el precio", () => {
     expect(calcularPrecioNeto(3, 20)).toEqual(60);
   });
+  it("debería retornar 'Cantidad inválida' si la cantidad es negativa ", () => {
+    expect(calcularPrecioNeto(-5, 20)).toEqual("Cantidad inválida");
+  });
+  it("debería retornar 'Precio inválido' si el precio es negativo", () => {
+    expect(calcularPrecioNeto(3, -10)).toEqual("Precio inválido");
+  });
 });
